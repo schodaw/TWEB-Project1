1a1fb864b3d6bff960093c9e2e5104044a66acfa
qdung <quangdung.ngo@heig-vd.ch>
2014-11-24 15:27:12 +0100

    Comments for the models API

--- server/api/lecture/lecture.model.js
+++ server/api/lecture/lecture.model.js
@@ -1,3 +1,13 @@
+/*
+ * Model of a current lecture en live
+ *
+ * Components of a lecture :
+ * - title : title of the lecture
+ * - author : author of the lecture
+ * - pdfPath : where the PDF file is stored
+ * - currentPage : number of the current page
+ */
+
 'use strict';
 
 var mongoose = require('mongoose'),
